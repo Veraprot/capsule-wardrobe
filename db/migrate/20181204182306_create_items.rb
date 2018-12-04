b771e762d2431b06d81acde6df1614fc73b30cfd
minheekangg <minheekangg@gmail.com>
2018-12-04 13:47:40 -0500

    add models

---
+++ db/migrate/20181204182306_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :times_worn
+      t.string :image
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
