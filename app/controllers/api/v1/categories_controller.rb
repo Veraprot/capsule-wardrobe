a828ab39e9edc344392a5138886f75958f33d95f
Veraprot <vera.protopopova@gmail.com>
2018-12-04 15:44:16 -0500

    add create and show actions

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,4 +3,25 @@ class Api::V1::CategoriesController < ApplicationController
     @categories = Category.all
     render json: @categories, status: :ok
   end 
+
+  def show 
+    @category = Category.find(params[:id])
+    render json: @category, status: :ok
+  end 
+
+  # post 
+  def create
+    @category = Category.create(category_params)
+    if @category.valid?
+      render json: @category, status: :created
+    else
+      render json: @category.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
